105893558da239abebf1f92ac487f9834101cefe
Francesco Fedele <56889513+kal1gh0st@users.noreply.github.com>
2021-08-15 17:07:00 +0200

    Update __init__.py

--- instagram-py/InstagramPy/colors/__init__.py
+++ instagram-py/InstagramPy/colors/__init__.py
@@ -1,7 +1,8 @@
 # Copyright Jonathan Hartley 2013. BSD 3-Clause license, see LICENSE file.
+# i="1"; echo -e '#!/usr/bin/env bash\nmyprogram -i '"\"input_${i}.txt\""
 from .initialise import init, deinit, reinit, colorama_text
 from .ansi import Fore, Back, Style, Cursor
 from .ansitowin32 import AnsiToWin32
 
-__version__ = '0.3.9'
+__version__ = '0.3.11'
 
